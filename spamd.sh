4b5366bf6ba799856c1d4a7c5431230107f994b6
Christian Luginbühl <dinkel@pimprecords.com>
2015-02-08 16:43:46 +0100

    Reverting last commit ... that  went into the wrong repo

--- spamd.sh
+++ spamd.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# This is a hack, because 'spamd_address' does not expand hostnames (only in Exim >= 4.85)
-spamd_ip=`grep 'spamassassin' /etc/hosts | cut -f1`
-sed -i "s/spamd_address = \(.*\)$/spamd_address = $spamd_ip 783/g" /etc/exim4/exim4.conf
-
 spamd --username debian-spamd \
       --nouser-config \
       --syslog stderr \
