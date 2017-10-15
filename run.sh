5ef2a4993871c5586c77c2b9317239d0549aba1d
Rainer Rillke <rillke@wikipedia.de>
2017-10-15 17:04:26 +0200

    Attempt graceful shutdown of all processes
    Forward SIGTERM to all the processes running in the container, then exit.

--- run.sh
+++ run.sh
@@ -8,20 +8,57 @@ pids=`jobs -p`
 
 exitcode=0
 
+# kindly ask my children to exit
 function terminate() {
+    # unbind handler
     trap "" CHLD
-
+    # iterate over previously captured PIDs,
+    # each identifying a process
     for pid in $pids; do
+        # check whether we have permission to
+        # kill that process, and if we do not
+        # wait for that process to end
+        # if we have permission, loop simply
+        # continues
         if ! kill -0 $pid 2>/dev/null; then
             wait $pid
             exitcode=$?
         fi
     done
 
+    # if we are here, we have permission to
+    # kill all processes identified by $pids
     kill $pids 2>/dev/null
+    kill ${!}
+    # wait until all children have gone
+    wait
 }
 
-trap terminate CHLD
-wait
+# CHLD-handler
+function term_child_handler() {
+    terminate
+    exit $exitcode
+}
+
+# SIGTERM-handler
+function term_handler() {
+    terminate
+    exit 143; # 128 + 15 -- SIGTERM
+}
+
+# setup handlers
+
+# does one if the child processes exit?
+trap term_child_handler CHLD
+
+# on callback, kill the background process,
+# which is `tail -f /dev/null` and execute the specified handler
+trap 'kill ${!}; term_handler' SIGTERM
+
+# wait forever
+while true
+do
+    tail -f /dev/null & wait ${!}
+done
 
 exit $exitcode
